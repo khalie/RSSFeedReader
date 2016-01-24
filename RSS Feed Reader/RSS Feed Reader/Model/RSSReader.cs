9d4f9072cc3dcb97d5a2e09e48729c91e279d693
khalie <khalielol@gmail.com>
2016-01-24 10:53:31 +0100

    Added the RSSReader class, which loads a defined RSS Feed and returns a List of SyndicationFeed Items.

---
+++ RSS Feed Reader/RSS Feed Reader/Model/RSSReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Web.Syndication;
+
+namespace RSS_Feed_Reader.Model
+{
+  public class RSSReader
+  {
+    /// <summary>
+    /// Creates the syndicationClient Object that loads the defined News Feed.
+    /// </summary>
+    /// <returns>List of Syndication Items</returns>
+    public async Task<IList<SyndicationItem>> LoadNewsAsync()
+    {
+      SyndicationClient syndicationClient = new SyndicationClient();
+      var feeds = await syndicationClient.RetrieveFeedAsync(new Uri("http://blog.pdapda.de/feed/"));
+
+      return feeds.Items;
+    }
+  }
+}
